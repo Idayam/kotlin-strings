44f952cc92a29dbf26c65a9efbde55a951e799d1
Idayam <mzeeidaya@gmail.com>
2023-02-17 08:48:47 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val word = "akirachix"
+    println(charArrayOf(word[0],word[2],word[3]))
+
+
+
+    val name = "Idaya"
+    val age = "20"
+    val sentence = "Hi,name is $name and I am $age years old "
+    println(sentence)
+
+
+    val place = "Mombasa"
+    println(place.length)
+
+
+    val text="halima"
+    val text2="Idaya"
+    if (text.equals(text2)){
+        println("Thats me!")}else{
+        println("I don't know who that is")}
+
+
+
+}
+
+
+
+
+
